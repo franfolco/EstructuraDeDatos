552dd91a2ee4a836565054b119cc91ed9f54be94
franfolco <franfolcox@gmail.com>
2023-08-22 18:23:47 -0300

    Update ejercicio 1.py

--- ejercicio 1.py
+++ ejercicio 1.py
@@ -18,3 +18,10 @@ print("var2:", var2)
 print(var1+var2, "\n\n")
 #El codigo concatena dos variables con contenido de texto y nuevamente imprime por pantalla el string.
 
+
+
+print("Ejercicio 3")
+var1=input("¿Cual es tu nombre?")
+var2=input("¿Cual es tu DNI?")
+print("El nombre de usuario es: ",var1," \ny su DNI es: ", var2)
+#El codigo solicita al usuario ingresar su nombre y dni y luego los imprime por pantalla.Fran
