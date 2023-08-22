10cdd138a513127b9296f52f4e085fa9a1a16b26
franfolco <franfolcox@gmail.com>
2023-08-22 18:20:24 -0300

    Create ejercicio 1.py

---
+++ ejercicio 1.py
@@ -0,0 +1,11 @@
+#Ejercicio numero 1 TP PDF PYTHON
+
+print("ejercicio 1")
+var1=3
+var2=4
+print("var1:", var1)
+print("var2:", var2)
+print(var1+var2, "\n\n")
+#El codigo suma dos variables y imprime por pantalla el resultado.
+
+
