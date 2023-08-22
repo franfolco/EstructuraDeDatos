45048633e104c15c0da549aa7a2daac1280d4ba5
franfolco <franfolcox@gmail.com>
2023-08-22 18:22:57 -0300

    Update ejercicio 1.py

--- ejercicio 1.py
+++ ejercicio 1.py
@@ -9,3 +9,12 @@ print(var1+var2, "\n\n")
 #El codigo suma dos variables y imprime por pantalla el resultado.
 
 
+
+print("ejercicio 2")
+var1="hola"
+var2="mundo"
+print("var1:", var1)
+print("var2:", var2)
+print(var1+var2, "\n\n")
+#El codigo concatena dos variables con contenido de texto y nuevamente imprime por pantalla el string.
+
